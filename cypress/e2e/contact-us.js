79325b91a6cca51ab7ae167b6bd2e010459de41f
mnkollo <73963114+mnkollo@users.noreply.github.com>
2023-07-25 16:30:02 -0500

    completed my second test

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -13,7 +13,15 @@ describe('Test Contact Us form via WebdriverUni', () => {
         cy.get('h1').should('have.text', 'Thank You for your Message!');
     });
 
-    it('Should be able to submit a successful submission via contact us form', () => {
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
         //cypress code
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.get('h2').should('have.text', 'CONTACT US');
+        cy.get('[name="first_name"]').type('Joe');
+        //cy.get('[name="last_name"]').type('Blogs');
+        cy.get('[name="email"]').type('joe.blogs@mailinator.com');
+        cy.get('textarea.feedback-input').type('How can I learn Cypress?');
+        cy.contains('SUBMIT').click();
+        cy.get('body').should('have.text', '\n\n\n Error: all fields are required\n\n\n');
     });
 })
