e0dbeb0c69a4f95f514c7621e95a97bbb4c4a82a
mnkollo <73963114+mnkollo@users.noreply.github.com>
2023-07-28 13:03:16 -0500

    updated Allure Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+            return config;
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
   },
