02cade5dba3bcf4ab89c11625fae4f568184152e
mnkollo <73963114+mnkollo@users.noreply.github.com>
2023-08-24 20:39:26 -0500

    created alerts file

---
+++ cypress/e2e/WebDriverUni/js-alerts.cy.js
@@ -0,0 +1,38 @@
+/// <reference types='cypress' />
+
+
+describe('Handle js alerts', () => {
+
+    beforeEach('Open browser', () => {
+        cy.openwebDriverUni();
+        cy.contains('POPUP & ALERTS')
+        .invoke('removeAttr', 'target').click({ force: true })
+
+    });
+    it('Confirm js alert contains the correct text', () => {
+        //cypress code
+        cy.get('[id="button1"]').click();
+
+        cy.on('window:alert', (str) => {                        //callback function - str is the parameter
+            expect(str).to.equal('I am an alert box!')        //window:alert is a cypress event to auto accept alerts
+        })
+    });
+    it.only('Validate js confrm alert box works correctly when clicking ok', () => {
+        //cypress code
+        cy.get('[id="button4"]').click();
+        
+        cy.on('window:confirm', (str) => {                        
+            return true;          // return true to click ok
+        })
+        cy.get('[id="confirm-alert-text"]').contains('You pressed OK!')
+    });
+    it.only('Validate js confrm alert box works correctly when clicking cancel', () => {
+        //cypress code
+        cy.get('[id="button4"]').click();
+        
+        cy.on('window:confirm', (str) => {                        
+            return false;                                     // return false to click cancel
+        })                                                    //window:confirm is a cypress event to be able to click ok or cancel
+        cy.get('[id="confirm-alert-text"]').contains('You pressed Cancel!')
+    });
+})
